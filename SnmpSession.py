fd9ac1f4a9f958f782140f7ae61ca5cd35c3640d
ecn <ecn@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-01-21 15:55:40 +0000

    Still using SnmpSession synchronous API in one place
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/pynetsnmp@21665 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ SnmpSession.py
@@ -0,0 +1,46 @@
+__doc__ = "Backwards compatible API for SnmpSession"
+
+import netsnmp
+from ctypes import *
+
+class SnmpSession(object):
+    
+    def __init__(self, ip, port=161, timeout=2, retries=2, cmdLineArgs=()):
+        self.ip = ip
+        self.port = port
+        self.timeout = timeout
+        self.retries = retries
+        self.community = "public"
+        self._version = netsnmp.SNMP_VERSION_1
+        self.cmdLineArgs = cmdLineArgs
+
+    def setVersion(self, version):
+        if version.find('2') >= 0:
+            self._version = netsnmp.SNMP_VERSION_2c
+        else:
+            self._version = netsnmp.SNMP_VERSION_1
+
+    def get(self, oid):
+        "Synchronous get implementation"
+        self.session = netsnmp.Session(
+            version=self._version,
+            timeout=int(self.timeout*1e6),
+            retries=int(self.retries-1),
+            peername= '%s:%d' % (self.ip, self.port),
+            community=self.community,
+            community_len=len(self.community),
+            cmdLineArgs=self.cmdLineArgs
+            )
+        oid = tuple(map(int, oid.strip('.').split('.')))
+        self.session.open()
+        try:
+            return self.session.sget([oid])
+        finally:
+            self.session.close()
+
+if __name__ == '__main__':
+    session = SnmpSession('127.0.0.1', timeout=1.5, port=161)
+    session.community = 'public'
+    print session.get('.1.3.6.1.2.1.1.5.0')
+    session.community = 'xyzzy'
+    print session.get('.1.3.6.1.2.1.1.5.0')
