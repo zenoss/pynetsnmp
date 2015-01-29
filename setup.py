cf58c5d2196e5835f3e7dff0cffd3923c30e5f8d
kalombo <ngorshkov@telenet.ru>
2015-01-29 10:18:09 +0500

    removed generating constants from setup.py file.

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
-from distutils.command.build import build as _build
-from distutils.command.clean import clean as _clean
 from distutils.core import setup
-
-import os
-
-import genconstants
-
 from version import VERSION
-
-class clean(_clean):
-    def run(self):
-        if os.path.exists('/usr/include/net-snmp/library/snmp_api.h'):
-            for filename in "CONSTANTS.py", "CONSTANTS.pyc":
-                if os.path.exists(filename):
-                    os.remove(filename)
-                _clean.run(self)
-
-class build(_build):
-    def run(self):
-        if os.path.exists('/usr/include/net-snmp/library/snmp_api.h'):
-            genconstants.make_imports()
-            _build.run(self)
 
 if __name__=='__main__':
     setup(name='pynetsnmp',
@@ -28,7 +7,6 @@ if __name__=='__main__':
           description='ctypes wrapper for net-snmp',
           author='Eric C. Newton',
           author_email='ecn@zenoss.com',
-          cmdclass={'build': build, 'clean' : clean},
           package_dir = {'pynetsnmp':'.',},
           packages = ['pynetsnmp',],
           )
