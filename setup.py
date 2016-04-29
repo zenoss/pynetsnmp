437068f102f6a4c4e68cf2bfc68ebe7ec89033c6
kalombo <nogamemorebrain@gmail.com>
2016-04-29 12:22:28 +0500

    added ipaddr to dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
-from distutils.core import setup
+from setuptools import setup
 
 import os
 
 import genconstants
 
 from version import VERSION
+
 
 class clean(_clean):
     def run(self):
@@ -16,19 +17,21 @@ class clean(_clean):
                     os.remove(filename)
                 _clean.run(self)
 
+
 class build(_build):
     def run(self):
         if os.path.exists('/usr/include/net-snmp/library/snmp_api.h'):
             genconstants.make_imports()
             _build.run(self)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     setup(name='pynetsnmp',
           version=VERSION,
           description='ctypes wrapper for net-snmp',
           author='Eric C. Newton',
           author_email='ecn@zenoss.com',
-          cmdclass={'build': build, 'clean' : clean},
-          package_dir = {'pynetsnmp':'.',},
-          packages = ['pynetsnmp',],
+          cmdclass={'build': build, 'clean': clean},
+          package_dir={'pynetsnmp': '.'},
+          packages=['pynetsnmp'],
+          install_requires=['ipaddr']
           )
