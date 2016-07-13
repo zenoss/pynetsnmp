69f98fe10208a6c235d8898298e2da235254b91b
Glenn Jones <gjones@zenoss.com>
2016-07-13 16:31:22 -0500

    Bump version to 0.40.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pynetsnmp",
-    version = "0.40.6-dev",
+    version = "0.40.6",
     packages=find_packages(),
     install_requires = [
         'setuptools',
