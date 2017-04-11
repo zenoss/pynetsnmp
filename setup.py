3cc2d231671980973867ca4f879e7082576c7d8c
Jason Peacock <jpeacock@zenoss.com>
2017-04-11 12:07:37 -0500

    Update version to 0.40.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pynetsnmp",
-    version = "0.40.7-dev",
+    version = "0.40.7",
     packages=find_packages(),
     install_requires = [
         'setuptools',
