b767af96743b1d8eb6ca1b37696b76f2aa39fa08
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-09-04 14:19:55 +0200

    correctly add a dependency to ipaddr module under python 2

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import sys
+
 from setuptools import setup, find_packages
 
 # 'make build' will build the pynetsnmp and copy them to the needed locations.
+
+requires = [
+    'setuptools',
+]
+if sys.version_info[0] == 2:
+    requires.append('ipaddr')
 
 setup(
     name = "pynetsnmp",
     version = "0.40.8-dev",
     packages=find_packages(),
-    install_requires = [
-        'setuptools',
-    ],
+    install_requires = requires,
     include_package_data=True,
     # metadata for upload to PyPI
     author = "Zenoss",
