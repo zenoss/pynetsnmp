9dd15321647e22a88c70c195082255e3aa09d31d
kalombo <nogamemorebrain@gmail.com>
2017-11-01 09:29:25 +0500

    Bump version: 0.0.6 → 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pynetsnmp-2",
-    version="0.0.6",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         'setuptools',
